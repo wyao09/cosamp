f0559ee28a7382a0b58897fc5cac5b4d09e28d82
Willie Yao <wyao09@gmail.com>
2012-04-02 02:13:20 -0400

    fixed code

--- cosamp.c
+++ cosamp.c
@@ -91,61 +91,6 @@ main(int argc, char **argv){
 
   while((t < MAX_ITER) && 
 	(dnrm2_(&vector_size, v, &incx)/dnrm2_(&vector_size, u, &incx) > tol)){
-    
-  
-   %% pick top 2K components
-   [vals,z] = sort(y,'descend');
-   Omega = find(y >= vals(2*K) & y > numericalprecision);
-  
-   %% merge the selected indices with those from the previous iteration
-   T = union(Omega,T);
-
-   %% reduce the system sizem and solve least squares
-   b = pinv(Phi(:,T))*u;
-   
-   %% pick top K components
-   [vals,z] = sort(abs(b),'descend');
-   Kgoodindices = (abs(b) >= vals(K) & abs(b) > numericalprecision);  
-   T = T(Kgoodindices);
-   Sest = zeros(size(Phi,2),1);
-   b = b(Kgoodindices);
-   Sest(T) = b;
-  
-   %% compute residuals
-   v = u - Phi(:,T)*b;
-  
-   t = t+1;
-   end
-
-*/ 
-
-#include "blas.h"
-#include "f2c.h"
-#include <stdio.h>
-#include "clapack.h"
-
-#define MAX_ITER 10
-int i;
-
-main(int argc, char **argv){
-  /* given */
-  doublereal *Phi; //measurement matrix
-  integer vector_size;
-  doublereal *u; //measured vector
-  doublereal tol = 0.01; //tolerance for approx between successive solutions. 
-  /* end given */
-
-  //copy u to v
-  doublereal *v = malloc(vector_size*sizeof(u));
-  for(i=0;i<vector_size;i++){
-    v[i] = u[i];
-  }
-  
-  int t = 0;
-  integer incx = 1; // increment (usually 1)
-
-  while((t < MAX_ITER) && 
-	(dnrm2_(&vector_size, v, &incx)/dnrm2_(&vector_size, u, &incx) > tol)){
 
     // make a guess on nonzero locations
     y = abs(Phi* *v);
