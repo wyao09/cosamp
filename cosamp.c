22ec98e30ec6d93e828d5591e944966a3ba91cee
Willie Yao <wyao09@gmail.com>
2012-04-08 17:59:49 -0400

    mid implementation commit

--- cosamp.c
+++ cosamp.c
@@ -1,68 +1,3 @@
-/* Matlab Implementation here:
-
-   % Cosamp algorithm
-   %   Input
-   %       K : sparsity of Sest
-   %       Phi : measurement matrix
-   %       u: measured vector
-   %       tol : tolerance for approximation between successive solutions. 
-   %   Output
-   %       Sest: Solution found by the algorithm
-   %
-   % Algorithm as described in "CoSaMP: Iterative signal recovery from 
-   % incomplete and inaccurate samples" by Deanna Needell and Joel Tropp.
-   % 
-
-   % This script creates a random signal S, and samples the signal with Phi.
-   % Later it uses Cosamp to recover the random signal.
-
-   S = zeros(1000,1);
-   K = 10;
-   S(1:K) = rand(K,1);
-   Phi = rand(100,1000);
-   Phi(Phi >= 0.5) = 1;
-   Phi(Phi < 0.5) = -1;
-   Phi = Phi/sqrt(100);
-   u = Phi*S;
-
-   tol = 0.01;
-   maxiterations = 10;
-
-   Sest = zeros(size(Phi,2),1);
-   v = u;
-   t = 1; 
-   numericalprecision = 1e-12;
-   T = [];
-
-   while (t <= maxiterations) && (norm(v)/norm(u) > tol)
-   %% make a guess on nonzero locations
-   y = abs(Phi'*v);
-  
-   %% pick top 2K components
-   [vals,z] = sort(y,'descend');
-   Omega = find(y >= vals(2*K) & y > numericalprecision);
-  
-   %% merge the selected indices with those from the previous iteration
-   T = union(Omega,T);
-
-   %% reduce the system sizem and solve least squares
-   b = pinv(Phi(:,T))*u;
-   
-   %% pick top K components
-   [vals,z] = sort(abs(b),'descend');
-   Kgoodindices = (abs(b) >= vals(K) & abs(b) > numericalprecision);  
-   T = T(Kgoodindices);
-   Sest = zeros(size(Phi,2),1);
-   b = b(Kgoodindices);
-   Sest(T) = b;
-  
-   %% compute residuals
-   v = u - Phi(:,T)*b;
-  
-   t = t+1;
-   end
-
-*/ 
 #include <stdlib.h>
 #include "blaswrap.h"
 #include "f2c.h"
@@ -72,18 +7,32 @@
 #define MAX_ITER 10
 int i;
 
+int cmp (const void *a, const void *b){
+  doublereal pa = *(const doublereal*) a;
+  doublereal pb = *(const doublereal*) b;
+  pa = pa - pb;
+  if(pa == 0)
+    return 0;
+  if(pa > 0)
+    return 1;
+  return 0;
+}
+
 main(int argc, char **argv){
   /* given */
-  doublereal *Phi; //measurement matrix
-  integer m;
-  integer n; //aka vector_size?
-  doublereal *u; //measured vector
-  integer vector_size;
-  doublereal tol = 0.01; //tolerance for approx between successive solutions. 
+  int k = 10;//sparsity
+  integer m = 4;
+  integer n = 6; //aka vector_size?
+  doublereal Phi[m*n]; //measurement matrix
+  integer vector_size = m;
+  doublereal u[m]; //measured vector
+  doublereal tol = 0.01; //tolerance for approx between successive solns. 
   /* end given */
 
+  doublereal *T = malloc(vector_size*sizeof(doublereal));
+
   //copy u to v
-  doublereal *v = malloc(vector_size*sizeof(u));
+  doublereal *v = malloc(vector_size*sizeof(doublereal));
   for(i=0;i<vector_size;i++){
     v[i] = u[i];
   }
@@ -91,24 +40,55 @@ main(int argc, char **argv){
   int t = 0;
   integer incx = 1; // increment (usually 1)
 
-  while((t < MAX_ITER) && 
+  char trans = 'C';
+  doublereal alpha = 1;
+  doublereal beta = 0;
+  integer lda = 1;
+  // does this actually need to be malloced? look at abs
+  doublereal *y = malloc(vector_size*sizeof(doublereal));
+
+
+  integer info;
+  integer nrhs = 1; // number of columns of matrices B and X in B = AX
+  doublereal *work = (double*)malloc( lwork*sizeof(doublereal) );
+  integer la = max(m,n);
+  integer lb = m;
+
+  WHILE((t < MAX_ITER) && 
 	(dnrm2_(&vector_size, v, &incx)/
 	 dnrm2_(&vector_size, u, &incx) > tol)){
 
-    
-
-    char trans = 'C';
-    doublereal alpha = 1;
-    doublereal beta = 0;
-    integer lda = 1;
-    // does this actually need to be malloced? look at abs
-    doublereal *y = malloc(vector_size*sizeof(doublereal));
-
     // Phi* *v
+    trans = 'C';
     dgemv_(&trans,&m,&vector_size,&alpha,v,&lda,Phi,&incx,&beta,y,&incx);
     
     // y = abs(Phi* *v)
     // may have to loop with dcabs1_(doublecomplex z)
+    for (i=0;i<vector_size;i++){
+      if(y[i] < 0)
+	y[i] = -1 * y[i];
+    }
+
+    // sort y
+    qsort(y, vector_size, sizeof(doublereal), cmp);
+    
+    // merge top 2k with T (this can be a lot better)
+    // may need to change to indicies
+    for(i=k;i<3*k;i++){
+      T[i] = y[i];
+    }
+    qsort(T, 3*k, sizeof(doublereal), cmp);
+
+    
+    // reduce system size and solve for least square
+    // ? = Phi y
+    trans = 'N';
+    dgels_(&trans,&m,&n,&nrhs,Phi,la,T,lb,work,lork,&info);
+
+    // pick top K components of b(aka T aka x_), "set" others to 0
+
+    // compute residual
+    
 
     t++;
   }
