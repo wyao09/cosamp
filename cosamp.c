91838ff971c1da2e1bd58041a684dff04f04167e
Willie Yao <wyao09@gmail.com>
2012-04-10 11:24:53 -0400

    print in right order. need to fix sorting tuple

--- cosamp.c
+++ cosamp.c
@@ -4,29 +4,41 @@
 #include <stdio.h>
 #include "clapack.h"
 
-#define MAX_ITER 10
+#define MAX_ITER 1
 int i,j;
 
+typedef struct{
+  doublereal value;
+  int index;
+} tuple;
+
+// converts from column major order
 void print_matrix(doublereal *p, int m, int n){
   for(i=0;i<m;i++){
     for(j=0;j<n;j++){
-      printf("%f ",(double)p[j+i*n]);
+      printf("%f ",(double)p[i+j*m]);
     }
     printf("\n");
   }
   printf("\n");
 }
 
-
-int cmp (const void *a, const void *b){
-  doublereal pa = *(const doublereal*) a;
-  doublereal pb = *(const doublereal*) b;
-  pa = pa - pb;
-  if(pa == 0)
+//need to optimize this
+int cmp_tuple (const void *a, const void *b){
+  tuple pa = *(const tuple*) a;
+  tuple pb = *(const tuple*) b;
+  pa.value = pa.value - pb.value;
+  if(pa.value == 0)
     return 0;
-  if(pa > 0)
+  if(pa.value > 0)
     return 1;
   return 0;
+}
+
+//make bit array later/use better set implementation
+void reset(int *set, int n){
+  for(i=0;i<n;i++)
+    set[i] = 0;
 }
 
 main(int argc, char **argv){
@@ -65,16 +77,18 @@ main(int argc, char **argv){
   for(i=0;i<m;i++)
     u[i] = y_sample[i];
 
-  doublereal *T = malloc(3*n*sizeof(doublereal));// this should probably be 3k
+  int T[(int)n]; //stores indicies
+  reset(T,n);
 
   //copy u to v
-  doublereal *v = malloc(m*sizeof(doublereal));
+  doublereal *v = (doublereal*) malloc(m*sizeof(doublereal));
   for(i=0;i<m;i++){
     v[i] = u[i];
   }
-  
-  print_matrix(u,m,1);
 
+  doublereal b[m*n];
+
+  
   int t = 0;
   integer incx = 1; // increment (usually 1)
 
@@ -82,16 +96,17 @@ main(int argc, char **argv){
   doublereal alpha = 1;
   doublereal beta = 0;
   integer lda = m;//1;
-  // does this actually need to be malloced? look at abs
-  doublereal *y = malloc(n*sizeof(doublereal));
+  // need to keep original indicies
+  tuple *y = (tuple*) malloc(n*sizeof(tuple));
+  doublereal y_i[n];
 
   integer info;
   integer nrhs = 1; // number of columns of matrices B and X in B = AX
-  integer lwork = max(1,min(m,n) + max(min(m,n),nrhs));
-  doublereal *work = malloc( lwork*sizeof(doublereal));
+  integer lwork = m + n;// max(1,min(m,n) + max(min(m,n),nrhs));
+  doublereal *work = (doublereal*)  malloc( lwork*sizeof(doublereal));
   integer la = m;
-  integer lb = max(m,n);
-  doublereal *x = malloc(k*sizeof(doublereal));
+  integer lb = m;//max(m,n);
+  doublereal *x = (doublereal*) malloc(k*sizeof(doublereal));
  
   // COSAMP Starts Here
   while((t < MAX_ITER) && 
@@ -101,30 +116,49 @@ main(int argc, char **argv){
 
     // Phi* *v
     trans = 'C';
-    dgemv_(&trans,&m,&n,&alpha,Phi,&lda,v,&incx,&beta,y,&incx);
-    
-    print_matrix(y,n,1);
+    dgemv_(&trans,&m,&n,&alpha,Phi,&lda,v,&incx,&beta,y_i,&incx);
 
-    // y = abs(Phi* *v)
+    // y = abs(Phi* *v) and add index
     // may have to loop with dcabs1_(doublecomplex z)
     for (i=0;i<n;i++){
-      if(y[i] < 0)
-	y[i] = -1 * y[i];
+      if(y_i[i] < 0)
+	y[i].value = -1 * y_i[i];
+      else
+	y[i].value = y_i[i];
+      y[i].index = i;
     }
 
-
+    for(i=0;i<n;i++){
+      printf("%f %d\n",y[i].value, y[i].index);
+    }
 
     // sort y
-    qsort(y, n, sizeof(doublereal), cmp);
-    
+    qsort(y, n, sizeof(doublereal), cmp_tuple);
+
+
+
     // merge top 2k with T (this can be a lot better)
     // may need to change to indicies
-    for(i=k;i<3*k;i++){
-      T[i] = y[i];
+    for(i=0;i<2*k;i++){
+      T[y[i].index] = 1;
+      printf("%d ",y[i].index);
     }
-    qsort(T, 3*k, sizeof(doublereal), cmp);
-
+    printf("\n");
     
+    int k = 0;
+    for(i=0;i<n;i++){
+      for(j=0;j<m;j++){
+	if(T[j]){
+	  b[k] = Phi[i*n+j];
+	  k++;
+	}
+      }
+    }
+
+    //print_matrix(b,m,4);
+
+    /*
+
     // reduce system size and solve for least square
     // ? = Phi y
     // answer stored in T
@@ -147,6 +181,7 @@ main(int argc, char **argv){
       v[i] = u[i] - x[i];
     }
 
+    */
     //print_matrix(x,16,1);
 
     t++;
